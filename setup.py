c4e53c3e79ad4abd23b4566f60bc28ee859411af
João Pedro Leal <45352137+jpkleal@users.noreply.github.com>
2024-04-02 10:09:31 -0300

    feat(version): update version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="NeonPathPlanning",
     packages=find_packages(),
-    version="1.0.0",
+    version="1.0.1",
     description="Collection of autonomous robot path planners",
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
